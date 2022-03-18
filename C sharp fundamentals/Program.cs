1057e8b545fdb3109a540d3572b881764e82697d
Gbless Ugo <gbless1720@gmail.com>
2022-03-18 19:34:43 +0100

    equation commit

---
+++ C sharp fundamentals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C_sharp_fundamentals
+{
+    class Program
+    {
+        // y = I - (f ^ n[(s.l) / f + (20 / f) ^ w]) / 20 ^ n
+        static void Main(string[] args)
+        {
+            double y = 0.0;
+            double f = 0.0;
+            double n = 0.0;
+            double I = 0.0;
+            double w = 0.0;
+            double l = 0.0;
+            double b = Math.Pow(f, n);
+            double r = Math.Pow(20, n);
+            double q = 20 / f;
+            double s = Math.Pow(q, w);
+
+            y = I - b((s * l) / f + r * s);) / r;
+
+        }
+    }
+}
---
+++ Number 4/Number 4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Number_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 30;
+
+            if( a == b && c > a + b - c)
+            {
+                a = c - b;
+            }
+           
+            Console.WriteLine("a is {0}",a);
+        }
+    }
+}
